43f89d5339eefe7c22847844351a030e5cdeb322
Aakash kaushik <kaushikaakash7539@gmail.com>
2021-08-09 16:04:22 +0530

    installation for conda and packages

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Tools and tech stack for deep learning on windows
 ## Steps
 Open powershell as **admin** and go through the below commands to setup the tools
 
-```powershell
+1. Go [here](https://www.anaconda.com/products/individual) and download the anaconda installer for windows and place it in the same folder as the script and rename it to **Anaconda3**.
+
+2. ```powershell
 Set-ExecutionPolicy RemoteSigned
 ./setup.ps1
 Set-ExecutionPolicy Restricted
--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,9 @@
-﻿pip install torch
+﻿Write-Output "Installing Anaconda"
+# .\Anaconda3 /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /S /D=%UserProfile%\Miniconda3
+Write-Output "Creating conda environment named deep_learning"
+conda update conda -y
+conda create -n deep_learning python=3.7 -y
+activate deep_learning
+Write-Output "Installing essesntial packages for deep learning in the conda environment"
+pip install --no-input torch torchvision tensorflow scikit-learn pandas numpy nltk openvino-dev
+conda deactivate
